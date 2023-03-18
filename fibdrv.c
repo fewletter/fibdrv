4af48e5f0339e7c13a19563651eb5c768361f11b
fewletter <fewletter@gmail.com>
2023-03-18 16:32:42 +0800

    Modify fast doubling with built in function

--- fibdrv.c
+++ fibdrv.c
@@ -36,14 +36,13 @@ static long long fib_sequence(long long k)
     f[0] = 0;
     f[1] = 1;
 
-    for (uint32_t i = 1UL << 31; i; i >>= 1) {
-        long long n0 = f[0] * (f[1] * 2 - f[0]);
+    for (uint32_t i = 1UL << (31 - __builtin_clz(k)); i; i >>= 1) {
+        long long n0 = f[0] * ((f[1] << 1) - f[0]);
         long long n1 = f[0] * f[0] + f[1] * f[1];
 
         uint64_t mask = !(k & i) - 1;
         f[0] = (n0 & ~mask) + (n1 & mask);
         f[1] = (n0 & mask) + n1;
-        ;
     }
     return f[0];
 }
